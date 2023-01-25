6eec8f35245b4ab15278bde12b29d8e5fe922457
markkoos <markusimercado@gmail.com>
2023-01-25 13:12:49 -0800

    added a test file for rendering

---
+++ src/components/testDiv.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function TestDiv() {
+    return (
+        <div>
+            <h1>This is a test function to display functionality of rendered elements</h1>
+            <ul>
+                <li>Hey</li>
+                <li>Lmao</li>
+            </ul>
+        </div>
+    )
+}
+
