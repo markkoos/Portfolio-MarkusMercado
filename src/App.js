9ba2556c0365aa615a42f4622fa3977590d0ac58
markkoos <markusimercado@gmail.com>
2023-01-25 14:16:47 -0800

    completed(for now) navbar with bootstrap and custom styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import AboutMe from "./components/pages/AboutMe";
+import NavBar from "./components/NavBar";
 
 export default function App() {
-  return <AboutMe />
+  return <NavBar />
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,1 +1,28 @@
 import React from 'react';
+
+export default function NavBar({ pageChangeHandler }) {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <span class="navbar-brand">Markus Mercado</span>
+            <ul className="navbar-nav mr-auto">
+
+                <li className="nav-item">
+                <a href="#aboutme" onClick={() => pageChangeHandler('About Me')}>About Me</a>
+                </li>
+
+                <li className="nav-item">
+                <a href="projects" onClick={() => pageChangeHandler('Projects')}>Projects</a>
+                </li>
+
+                <li className="nav-item">
+                <a href="#Resume" onClick={() => pageChangeHandler('Resume')}>Resume</a>
+                </li>
+                
+                <li className="nav-item">
+                <a href="#contactme" onClick={() => pageChangeHandler('Contact Me')}>Contact Me</a>
+                </li>
+
+            </ul>
+        </nav>
+    )
+}
