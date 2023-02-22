192ca2cb54fb6132e6cf600696d89607f6b6b934
markkoos <markusimercado@gmail.com>
2023-02-22 13:25:09 -0800

    added one project to the projects page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,9 +1,16 @@
 import React from "react";
+import doggy from "../../assets/images/doggydaycare.png"
 
 export default function Projects() {
     return (
         <div>
-            <h1>Featured Projects</h1>
+            <br />
+            <center><h1>Featured Projects</h1></center>
+            <br />
+
+            <center><h3>Doggy Daycare</h3></center>
+            <center><a className="projectImg" href="https://lit-everglades-07230.herokuapp.com/"><img src={doggy} className="doggy" alt="doggy_daycare"></img></a></center>
         </div>
+
     )
 }
