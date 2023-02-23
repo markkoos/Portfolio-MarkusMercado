e4a8e85d1cdbe976a1a6d16c571a013ac408aca2
Markus Mercado <markusimercado@gmail.com>
2023-02-22 18:43:09 -0800

    added contact information

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -3,7 +3,16 @@ import React from 'react';
 export default function ContactMe() {
     return (
         <div>
-            <h1>Contact Me</h1>
+            <br />
+            <center><h2 className="contactTitle">Contact Information</h2></center>
+            <br />
+
+            <ul>
+                <center><li>Email: markusimercado@gmail.com</li></center>
+                <center><li>Phone: (949) 275-6056</li></center>
+                <center><li>LinkedIn: <a className="contactLink" href="https://www.linkedin.com/in/markus-mercado-882609189/">https://www.linkedin.com/in/markus-mercado-882609189/</a></li></center>
+                <center><li>GitHub: <a className="contactLink" href="https://github.com/markkoos">https://github.com/markkoos</a></li></center>
+            </ul>
         </div>
     )
 }
